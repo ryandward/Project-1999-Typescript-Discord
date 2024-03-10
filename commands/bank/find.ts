3cb18060f8823e05044f34826c23afd09c8f66f4
Ryan David Ward <ryandward@gmail.com>
2024-03-09 22:13:33 -0600

    Refactor code to improve performance and readability

--- commands/bank/find.ts
+++ commands/bank/find.ts
@@ -6,7 +6,6 @@ import {
   EmbedBuilder,
   SlashCommandBuilder,
 } from 'discord.js';
-import _ from 'lodash';
 import { AppDataSource } from '../../app_data.js';
 import { Bank } from '../../entities/Bank.js';
 import {
@@ -23,7 +22,7 @@ export const data = new SlashCommandBuilder()
   .addStringOption(option =>
     option
       .setName('item')
-      .setDescription('Item name to search for')
+      .setDescription('The name of the item to search for')
       .setRequired(true)
       .setAutocomplete(true),
   );
@@ -31,9 +30,7 @@ export const data = new SlashCommandBuilder()
 export async function autocomplete(interaction: AutocompleteInteraction) {
   try {
     const focusedOption = interaction.options.getFocused(true);
-    if (!focusedOption) return;
-
-    if (focusedOption.name === 'item') {
+    if (focusedOption && focusedOption.name === 'item') {
       const searchTerm = `%${focusedOption.value}%`;
       const items = await AppDataSource.manager
         .createQueryBuilder(Bank, 'item')
@@ -44,7 +41,7 @@ export async function autocomplete(interaction: AutocompleteInteraction) {
         .limit(10)
         .getRawMany();
 
-      return await interaction.respond(
+      await interaction.respond(
         items.map(item => ({
           name: `(${item.Count}x) ${item.Name}`,
           value: item.Name,
@@ -53,127 +50,107 @@ export async function autocomplete(interaction: AutocompleteInteraction) {
     }
   }
   catch (error) {
-    if (error instanceof Error) {
-      console.error('Error in autocomplete:', error);
-    }
+    console.error('Error in autocomplete:', error);
   }
 }
 
 export async function execute(interaction: CommandInteraction) {
   try {
-    const { options } = interaction;
-
-    if (!options.get('item')) {
-      throw new Error('You must provide an item to find.');
+    const itemName = interaction.options.get('item', true).value as string;
+    const itemData = await AppDataSource.manager.find(Bank, {
+      where: { Name: itemName },
+    });
+    if (itemData.length === 0) {
+      await interaction.reply('Item not found.');
+      return;
     }
-    else {
-      const itemName = options.get('item')?.value as string;
 
-      const itemData = await AppDataSource.manager.find(Bank, {
-        where: { Name: itemName },
+    let itemText =
+      itemName.startsWith('Spell: ') || itemName.startsWith('Song: ')
+        ? `${await getSpellLevels(itemName)}\n\n${await getSpellDescription(itemName)}`
+        : await getItemStatsText(itemName);
+
+    if (!itemText) {
+      await interaction.reply('Item not found.');
+      return;
+    }
+
+    itemText = itemText.replace(/\[\[[^\]]*\|([^\]]+)\]\]/g, '$1');
+    itemText = itemText.replace(/(.{1,45})(\s|$)/g, '$1\n');
+
+    const lineHeight = 25;
+    const padding = 50;
+    const lines = itemText.split('\n');
+    const textHeight = lines.length * lineHeight;
+    const canvasHeight = Math.max(textHeight + padding, 200);
+    const canvas = Canvas.createCanvas(700, canvasHeight);
+    const context = canvas.getContext('2d');
+
+    const background = await Canvas.loadImage('./images/stars.png');
+    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    const imageUrl = await getImageUrl(itemName);
+    if (imageUrl) {
+      const icon = await Canvas.loadImage(imageUrl);
+      context.drawImage(icon, canvas.width - 100, 25, 75, 75);
+      context.font = `${lineHeight}px sans-serif`;
+      context.fillStyle = '#ffffff';
+      lines.forEach((line, i) => {
+        context.fillText(line, 25, 25 + i * lineHeight);
       });
+    }
 
-      // check if the itemName is a spell or a song
-      let itemText: string | null | undefined = '';
+    const attachment = new AttachmentBuilder(await canvas.encode('png'), {
+      name: 'item-stats-image.png',
+    });
 
-      if (itemName.startsWith('Spell: ') || itemName.startsWith('Song: ')) {
-        const spellLevels = await getSpellLevels(itemName);
-        const spellDescription = await getSpellDescription(itemName);
-        itemText = `${spellLevels}\n\n${spellDescription}`;
-        // wrap newlines every 50 characters, but don't break words
-      }
-      else {
-        itemText = await getItemStatsText(itemName);
-      }
+    const isStack = itemData.some(item => item.Quantity > 1);
+    const bankers = itemData.map(item => item.Banker);
 
-      if (!itemText) {
-        await interaction.reply('Item not found.');
-        return;
-      }
+    const embed = new EmbedBuilder()
+      .setTitle(':bank: Bank Record')
+      .setDescription(`**${itemName}**\n<t:${Math.floor(Date.now() / 1000)}:R>`)
+      .setColor('Green')
+      .setImage('attachment://item-stats-image.png');
 
-      itemText = itemText?.replace(/\[\[[^\]]*\|([^\]]+)\]\]/g, '$1');
-      itemText = itemText?.replace(/(.{1,45})(\s|$)/g, '$1\n');
+    const embedBuilder = [...new Set(bankers)].reduce((currentEmbed, banker) => {
+      const itemsOnBankers = itemData.filter(item => item.Banker === banker);
+      const sortedItems = itemsOnBankers.sort((a, b) => a.Location.localeCompare(b.Location));
+      const sortedItemLocations = formatField(
+        sortedItems.map(item => item.Location.replace('-', ' ')),
+      );
 
-      const lineHeight = 25;
-      const padding = 50;
-      const lines = itemText.split('\n');
-      const textHeight = lines.length * lineHeight;
-      const canvasHeight = _.max([textHeight + padding, 200]) as number;
-      const canvas = Canvas.createCanvas(700, canvasHeight);
-      const context = canvas.getContext('2d');
-
-      const background = await Canvas.loadImage('./images/stars.png');
-
-      context.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-      const imageUrl = await getImageUrl(itemName);
-
-      if (imageUrl) {
-        try {
-          const icon = await Canvas.loadImage(imageUrl);
-          context.drawImage(icon, canvas.width - 100, 25, 75, 75);
-          if (itemText) {
-            context.font = `${lineHeight}px sans-serif`;
-            context.fillStyle = '#ffffff';
-            lines.forEach((line, i) => {
-              context.fillText(line, 25, 25 + i * lineHeight + 25);
-            });
-          }
-        }
-        catch (error) {
-          console.error('Failed to load image:', error);
-          return;
-        }
-      }
-
-      const attachment = new AttachmentBuilder(await canvas.encode('png'), {
-        name: 'item-stats-image.png',
-      });
-
-      const isStack = itemData.some(item => item.Quantity > 1);
-      const bankers = itemData.map(item => item.Banker);
-
-      const embed = new EmbedBuilder()
-        .setTitle(':bank: Bank Record')
-        .setDescription(`**${itemName}**\n<t:${Math.floor(Date.now() / 1000)}:R>`)
-        .setColor('Green')
-        .setImage('attachment://item-stats-image.png');
-
-      const embedBuilder = [...new Set(bankers)].reduce((currentEmbed: EmbedBuilder, banker) => {
-        const itemsOnBankers = itemData.filter(item => item.Banker === banker);
-        const sortedItems = itemsOnBankers.sort((a, b) => a.Location.localeCompare(b.Location));
-        const sortedItemLocations = formatField(
-          sortedItems.map(item => _.replace(item.Location, '-', ' ')),
-        );
-
-        if (sortedItems.length === 0) {
-          return currentEmbed;
-        }
-
-        currentEmbed.addFields(
-          {
-            name: `:bust_in_silhouette: ${banker}`,
-            value: `${sortedItems.length} matching item(s).`,
-            inline: false,
-          },
-          { name: ':mag: Location', value: sortedItemLocations, inline: true },
-        );
-        if (isStack) {
-          currentEmbed.addFields({
-            name: ':money_bag: Stack',
-            value: formatField(sortedItems.map(item => item.Quantity.toString())),
-            inline: true,
-          });
-        }
+      if (sortedItems.length === 0) {
         return currentEmbed;
-      }, embed);
+      }
 
-      await interaction.reply({ embeds: [embedBuilder], files: [attachment] });
-    }
+      currentEmbed.addFields(
+        {
+          name: `:bust_in_silhouette: ${banker}`,
+          value: `${sortedItems.length} matching item(s)`,
+          inline: false,
+        },
+        { name: ':mag: Location', value: sortedItemLocations, inline: true },
+      );
+      if (isStack) {
+        currentEmbed.addFields({
+          name: ':money_bag: Stack',
+          value: formatField(sortedItems.map(item => item.Quantity.toString())),
+          inline: true,
+        });
+      }
+      return currentEmbed;
+    }, embed);
+
+    await interaction.reply({ embeds: [embedBuilder], files: [attachment] });
   }
-  catch (error) {
+  catch (error: unknown) {
+    console.error('Error in execute:', error);
     if (error instanceof Error) {
       await interaction.reply(`Error in execute: ${error.message}`);
+    }
+    else {
+      await interaction.reply('An unknown error occurred.');
     }
   }
 }
--- commands/bank/item_functions.ts
+++ commands/bank/item_functions.ts
@@ -16,12 +16,22 @@ interface MediaWikiResponse {
 export function formatField(field: string[]): string {
   return '```\n' + field.join('\n') + '\n```';
 }
-export async function getImageUrl(itemName: string): Promise<string | null> {
-  // Standardize the item name to ensure cache consistency
-  const standardizedItemName = itemName.replace('Song: ', '').replace('Spell: ', '');
 
-  // If not in cache, proceed to fetch the image URL
-  const baseUrl = 'http://localhost/mediawiki/api.php';
+const baseUrl = 'http://localhost/mediawiki/api.php';
+
+async function fetchMediaWikiResponse(params: URLSearchParams): Promise<MediaWikiResponse | null> {
+  try {
+    const response = await axios.get<MediaWikiResponse>(baseUrl, { params });
+    return response.data;
+  }
+  catch (error) {
+    console.error('Error fetching from MediaWiki:', error);
+    return null;
+  }
+}
+
+export async function getImageUrl(itemName: string): Promise<string | null> {
+  const standardizedItemName = itemName.replace(/(Song: |Spell: )/g, '');
   const searchParams = new URLSearchParams({
     action: 'query',
     prop: 'revisions',
@@ -30,68 +40,50 @@ export async function getImageUrl(itemName: string): Promise<string | null> {
     format: 'json',
   });
 
-  try {
-    const searchResponse = await axios.get<MediaWikiResponse>(baseUrl, { params: searchParams });
-    const pageId = Object.keys(searchResponse.data.query.pages)[0];
-    const pageData = searchResponse.data.query.pages[pageId];
+  const searchResponse = await fetchMediaWikiResponse(searchParams);
+  if (!searchResponse) return null;
 
-    if (!pageData.revisions) {
-      console.log(searchResponse.data);
-      return null;
-    }
+  const pageId = Object.keys(searchResponse.query.pages)[0];
+  const pageData = searchResponse.query.pages[pageId];
+  if (!pageData.revisions) return null;
 
-    const lucyImgIdMatch = pageData.revisions[0]['*'].match(/lucy_img_ID\s*=\s*(\d+)/);
-    const spelliconMatch = pageData.revisions[0]['*'].match(/spellicon\s*=\s*(\w+)/);
+  const content = pageData.revisions[0]['*'];
+  const lucyImgIdMatch = content.match(/lucy_img_ID\s*=\s*(\d+)/);
+  const spelliconMatch = content.match(/spellicon\s*=\s*(\w+)/);
 
-    let filename;
-    if (lucyImgIdMatch) {
-      const imageId = lucyImgIdMatch[1];
-      filename = `item_${imageId}.png`;
-    }
-    else if (spelliconMatch) {
-      const imageId = spelliconMatch[1];
-      filename = `Spellicon_${imageId}.png`;
-    }
-    else {
-      return null;
-    }
-
-    const imageInfoParams = new URLSearchParams({
-      action: 'query',
-      prop: 'imageinfo',
-      titles: `File:${filename}`,
-      iiprop: 'url',
-      format: 'json',
-    });
-
-    const imageInfoResponse = await axios.get<MediaWikiResponse>(baseUrl, {
-      params: imageInfoParams,
-    });
-    const imagePageId = Object.keys(imageInfoResponse.data.query.pages)[0];
-    const imagePageData = imageInfoResponse.data.query.pages[imagePageId];
-
-    if (imagePageData.imageinfo) {
-      let imageUrl = imagePageData.imageinfo[0].url;
-      imageUrl = imageUrl.replace('http://localhost:80/mediawiki/images', '/var/lib/mediawiki');
-
-      // Cache the image URL for future use
-
-      return imageUrl;
-    }
+  let filename;
+  if (lucyImgIdMatch) {
+    filename = `item_${lucyImgIdMatch[1]}.png`;
   }
-  catch (error) {
-    console.error('Error fetching image URL:', error);
+  else if (spelliconMatch) {
+    filename = `Spellicon_${spelliconMatch[1]}.png`;
+  }
+  else {
     return null;
   }
 
-  return null;
+  const imageInfoParams = new URLSearchParams({
+    action: 'query',
+    prop: 'imageinfo',
+    titles: `File:${filename}`,
+    iiprop: 'url',
+    format: 'json',
+  });
+
+  const imageInfoResponse = await fetchMediaWikiResponse(imageInfoParams);
+  if (!imageInfoResponse) return null;
+
+  const imagePageId = Object.keys(imageInfoResponse.query.pages)[0];
+  const imagePageData = imageInfoResponse.query.pages[imagePageId];
+  if (!imagePageData.imageinfo) return null;
+
+  let imageUrl = imagePageData.imageinfo[0].url;
+  imageUrl = imageUrl.replace('http://localhost:80/mediawiki/images', '/var/lib/mediawiki');
+  return imageUrl;
 }
 
 export async function getItemUrl(itemName: string): Promise<string | null> {
-  // Standardize the item name to ensure cache consistency
-  const standardizedItemName = itemName.replace('Song: ', '').replace('Spell: ', '');
-
-  const baseUrl = 'http://localhost/mediawiki/api.php';
+  const standardizedItemName = itemName.replace(/(Song: |Spell: )/g, '');
   const searchParams = new URLSearchParams({
     action: 'query',
     prop: 'info',
@@ -100,78 +92,49 @@ export async function getItemUrl(itemName: string): Promise<string | null> {
     format: 'json',
   });
 
-  const searchResponse = await axios.get<MediaWikiResponse>(baseUrl, { params: searchParams });
+  const searchResponse = await fetchMediaWikiResponse(searchParams);
+  if (!searchResponse) return null;
 
-  // Return the 'fullurl' property of the page
-  return (
-    searchResponse.data.query.pages[Object.keys(searchResponse.data.query.pages)[0]].fullurl || null
-  );
+  return searchResponse.query.pages[Object.keys(searchResponse.query.pages)[0]].fullurl || null;
 }
-export async function getItemStatsText(itemName: string): Promise<string | null> {
+
+async function fetchPageContent(url: string): Promise<string | null> {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.goto(url);
+  return page.content();
+}
+
+export async function getItemStatsText(itemName: string): Promise<string | null> {
   const itemUrl = await getItemUrl(itemName);
-  if (itemUrl) {
-    await page.goto(itemUrl);
-  }
-  else {
+  if (!itemUrl) {
     console.error('Item URL is null');
-    await browser.close();
     return null;
   }
 
-  // Primary stats element
-  const statsElement = await page.$(
-    '#mw-content-text > div.mw-parser-output > div.itembg > div > p',
-  );
+  const content = await fetchPageContent(itemUrl);
+  if (!content) return 'No stats found';
 
-  if (!statsElement) {
-    console.log('No stats element found with the given selector. Attempting fallback...');
-    return 'No stats found';
-  }
-  else {
-    const statsText = await page.evaluate(element => element.textContent || '', statsElement);
-    await browser.close();
-    return statsText.trim();
-  }
+  const statsTextMatch = content.match(/<div class="itembg">.*?<p>(.*?)<\/p>/s);
+  return statsTextMatch ? statsTextMatch[1].trim() : 'No stats found';
 }
 
 export async function getSpellLevels(spellName: string): Promise<string | null> {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
   const itemUrl = await getItemUrl(spellName);
-  if (itemUrl) {
-    await page.goto(itemUrl);
-  }
-  else {
+  if (!itemUrl) {
     console.error('Item URL is null');
-    await browser.close();
     return null;
   }
-  // Find the first two h2 elements
-  const h2Elements = await page.$$('h2');
-  let statsText = '';
 
-  // Ensure to only process the first two h2 elements
-  for (let i = 0; i < Math.min(h2Elements.length, 1); i++) {
-    // Get the next sibling element of each h2, which contains the desired content
-    const nextElement = await h2Elements[i].evaluateHandle(el => el.nextElementSibling);
+  const content = await fetchPageContent(itemUrl);
+  if (!content) return null;
 
-    // Extract text content if the element exists
-    const nextElementText = await (await nextElement.getProperty('textContent')).jsonValue();
-    statsText += nextElementText + '\n';
-  }
-
-  await browser.close();
-  return statsText.trim();
+  const spellLevelsMatch = content.match(/<h2>.*?<\/h2>(.*?)<h2>/s);
+  return spellLevelsMatch ? spellLevelsMatch[1].trim() : null;
 }
 
 export async function getSpellDescription(spellName: string): Promise<string | null> {
-  // Standardize the item name to ensure cache consistency
-  const standardizedItemName = spellName.replace('Song: ', '').replace('Spell: ', '');
-
-  // If not in cache, proceed to fetch the image URL
-  const baseUrl = 'http://localhost/mediawiki/api.php';
+  const standardizedItemName = spellName.replace(/(Song: |Spell: )/g, '');
   const searchParams = new URLSearchParams({
     action: 'query',
     prop: 'revisions',
@@ -180,26 +143,13 @@ export async function getSpellDescription(spellName: string): Promise<string | n
     format: 'json',
   });
 
-  try {
-    const searchResponse = await axios.get<MediaWikiResponse>(baseUrl, { params: searchParams });
-    const pageId = Object.keys(searchResponse.data.query.pages)[0];
-    const pageData = searchResponse.data.query.pages[pageId];
+  const searchResponse = await fetchMediaWikiResponse(searchParams);
+  if (!searchResponse) return null;
 
-    if (!pageData.revisions) {
-      console.log(searchResponse.data);
-      return null;
-    }
+  const pageId = Object.keys(searchResponse.query.pages)[0];
+  const pageData = searchResponse.query.pages[pageId];
+  if (!pageData.revisions) return null;
 
-    const spellDescription = pageData.revisions[0]['*'].match(/description\s*=\s*(.*)/);
-    if (spellDescription) {
-      return spellDescription[1];
-    }
-    else {
-      return null;
-    }
-  }
-  catch (error) {
-    console.error('Error fetching spell description:', error);
-    return null;
-  }
+  const spellDescriptionMatch = pageData.revisions[0]['*'].match(/description\s*=\s*(.*)/);
+  return spellDescriptionMatch ? spellDescriptionMatch[1] : null;
 }

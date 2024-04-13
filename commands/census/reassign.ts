209971eb23e3088d936e57c62d7cb6dd5f5c2519
Ryan David Ward <ryandward@gmail.com>
2024-04-13 15:49:42 -0500

    Add reassign or manage command to census/reassign.ts

---
+++ commands/census/reassign.ts
@@ -0,0 +1,150 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { AutocompleteInteraction, CommandInteraction } from 'discord.js';
+import _ from 'lodash';
+import { FindManyOptions, ILike } from 'typeorm';
+import { AppDataSource } from '../../app_data.js';
+import { Census } from '../../entities/Census.js';
+import { Status } from '../../entities/Status.js';
+import {
+  classMustExist,
+  declare,
+  insertUser,
+  levelMustBeValid,
+  toonMustExist,
+  userMustExist,
+  validCharacterClasses,
+} from './census_functions.js';
+
+const classNames = await validCharacterClasses();
+
+export async function getActiveStatuses() {
+  return (await AppDataSource.manager.find(Status)).filter(status => status.Status !== 'Dropped');
+}
+const activeStatuses = await getActiveStatuses();
+
+export async function statusMustBeActive(inputStatus: string) {
+  const statusEntered = activeStatuses.find(status => status.Status === inputStatus);
+  if (!statusEntered) throw new Error(`:x: ${inputStatus} is not a valid active status.`);
+  return statusEntered;
+}
+
+export const data = new SlashCommandBuilder()
+  .setName('reassign or manage')
+  .setDescription('reassign or manage a toon to a discord user')
+
+  .addUserOption(option =>
+    option
+      .setName('user')
+      .setDescription('The user to reassign or manage the toon to')
+      .setRequired(true),
+  )
+  .addStringOption(option =>
+    option
+      .setName('status')
+      .setDescription('The new status of the character')
+      .setRequired(true)
+      .addChoices(...activeStatuses.map(status => ({ name: status.Status, value: status.Status }))),
+  )
+  .addStringOption(option =>
+    option
+      .setName('name')
+      .setDescription(
+        'The name of the toon to reassign or manage. Autocomplete should not match any toons.',
+      )
+      .setRequired(true)
+      .setAutocomplete(true)
+      .setMaxLength(24),
+  )
+  .addNumberOption(option =>
+    option
+      .setName('level')
+      .setDescription('The level of the toon')
+      .setRequired(true)
+      .setMinValue(1)
+      .setMaxValue(60),
+  )
+  .addStringOption(option =>
+    option
+      .setName('class')
+      .setDescription('The class of the character')
+      .setRequired(true)
+      .addChoices(...classNames),
+  );
+
+export async function autocomplete(interaction: AutocompleteInteraction) {
+  try {
+    const focusedOption = interaction.options.getFocused(true);
+    if (!focusedOption) return;
+
+    if (focusedOption.name === 'name') {
+      const choices: FindManyOptions = {
+        where: {
+          Name: ILike(`%${focusedOption.value}%`),
+        },
+        take: 10,
+      };
+      const toons = await AppDataSource.manager.find(Census, choices);
+      await interaction.respond(toons.map(toon => ({ name: toon.Name, value: toon.Name })));
+    }
+  }
+  catch (error) {
+    console.error('Error in autocomplete:', error);
+  }
+}
+
+export const execute = async (interaction: CommandInteraction) => {
+  try {
+    const member = await interaction.guild?.members.fetch(interaction.user.id);
+    if (!member || !member.permissions.has(['ManageGuild'])) {
+      throw new Error('You do not have permission to use this command.');
+    }
+    let response: string;
+    const { options } = interaction;
+
+    let status = options.get('status')?.value as string;
+    const discordId = options.get('user')?.value as string;
+    const name = _.capitalize(options.get('name')?.value as string);
+    const characterClass = options.get('class')?.value as string;
+    const level = options.get('level')?.value as number;
+
+    if (!discordId) {
+      throw new Error('You must provide a discord user to reassign or manage the toon to.');
+    }
+
+    await Promise.all([
+      toonMustExist(name),
+      userMustExist(discordId),
+      classMustExist(characterClass),
+      levelMustBeValid(level),
+      statusMustBeActive(status),
+    ]);
+
+    let newUserResult = await insertUser(discordId);
+
+    if (newUserResult && status !== 'Main') {
+      status = 'Main';
+      newUserResult =
+        newUserResult +
+        '\n' +
+        `:warning: <@${discordId}>'s \`${name}\` was declared as \`Main\` since no \`Main\` was found.`;
+    }
+
+    const newToonResult = await declare(discordId, status, name, level, characterClass);
+
+    if (newUserResult) {
+      response = newUserResult + '\n' + newToonResult;
+    }
+    else {
+      response = newToonResult;
+    }
+    return interaction.reply(response);
+  }
+  catch (error) {
+    if (error instanceof Error) {
+      return interaction.reply({
+        content: error.message,
+        ephemeral: true,
+      });
+    }
+  }
+};

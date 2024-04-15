c8ee9a542164b373bf274821584fd680a89d9ebf
Ryan David Ward <ryandward@gmail.com>
2024-04-15 01:33:10 -0500

    Refactor account command to manage shared account login information

--- commands/utility/account.ts
+++ commands/utility/account.ts
@@ -1,28 +1,22 @@
 import { AutocompleteInteraction, CommandInteraction, SlashCommandBuilder } from 'discord.js';
-import _ from 'lodash';
 import { FindManyOptions, ILike } from 'typeorm';
 import { AppDataSource } from '../../app_data.js';
 import { SharedAccounts } from '../../entities/SharedAccounts.js';
-import { SharedToons } from '../../entities/SharedToons.js';
 
 export const permissions = ['ManageGuild'];
 
 export const data = new SlashCommandBuilder()
-  .setName('login')
-  .setDescription('Get or assign a toon to a login account')
+  .setName('account')
+  .setDescription('Get or set login information for a shared account')
   .addStringOption(option =>
     option
-      .setName('toon')
-      .setDescription('The name of the toon')
+      .setName('account')
+      .setDescription('The name of the account')
       .setRequired(true)
       .setAutocomplete(true),
   )
   .addStringOption(option =>
-    option
-      .setName('account')
-      .setDescription('The nameof the account')
-      .setRequired(false)
-      .setAutocomplete(true),
+    option.setName('password').setDescription('The password for the account').setRequired(false),
   );
 
 export async function autocomplete(interaction: AutocompleteInteraction) {
@@ -30,20 +24,6 @@ export async function autocomplete(interaction: AutocompleteInteraction) {
     const focusedOption = interaction.options.getFocused(true);
     if (!focusedOption) return;
 
-    if (focusedOption.name === 'toon') {
-      const choices: FindManyOptions = {
-        where: {
-          Name: ILike(`%${focusedOption.value}%`),
-        },
-        take: 10,
-      };
-      const toons = await AppDataSource.manager.find(SharedToons, choices);
-      await interaction.respond(
-        toons
-          .filter(toon => toon.Name !== null)
-          .map(toon => ({ name: toon.Name, value: toon.Name })),
-      );
-    }
     if (focusedOption.name === 'account') {
       const choices: FindManyOptions = {
         where: {
@@ -64,75 +44,63 @@ export async function autocomplete(interaction: AutocompleteInteraction) {
   }
 }
 
-export async function execute(interaction: CommandInteraction): Promise<void> {
+export async function execute(interaction: CommandInteraction) {
   const { options } = interaction;
-  const toonName = _.capitalize(options.get('toon')?.value as string);
-  const accountName = options.get('account')?.value as string;
+  const account = options.get('account')?.value as string;
+  const password = options.get('password')?.value as string;
 
   try {
-    let sharedToon = await AppDataSource.manager.findOneBy(SharedToons, { Name: toonName });
+    if (password) {
+      // Set or change the account/password combo
+      let sharedAccount: SharedAccounts | null = await AppDataSource.manager.findOneBy(
+        SharedAccounts,
+        {
+          Account: account,
+        },
+      );
+      let response = '';
 
-    if (!sharedToon && accountName) {
-      // No existing toon, create a new one
-      const sharedAccount = await AppDataSource.manager.findOneBy(SharedAccounts, {
-        Account: accountName,
-      });
       if (!sharedAccount) {
-        throw new Error(`No account found with the name \`${accountName}\`.`);
+        sharedAccount = new SharedAccounts();
+        sharedAccount.Account = account;
+        sharedAccount.Password = password;
+        response = `The password for account \`${account}\` has been set to \`${password}\`.`;
       }
-
-      sharedToon = new SharedToons();
-      sharedToon.Name = toonName;
-      sharedToon.Account = sharedAccount;
-      await AppDataSource.manager.save(sharedToon);
-
+      else {
+        const oldPassword = sharedAccount.Password;
+        sharedAccount.Account = account;
+        sharedAccount.Password = password;
+        response = `The password for account \`${account}\` has been updated from \`${oldPassword}\` to \`${password}\`.`;
+      }
+      await AppDataSource.manager.save(sharedAccount);
       await interaction.reply({
-        content: `A new toon \`${toonName}\` has been created and linked to account \`${accountName}\`.`,
-        ephemeral: false,
-      });
-    }
-    else if (sharedToon && accountName) {
-      // Existing toon, update the account
-      const sharedAccount = await AppDataSource.manager.findOneBy(SharedAccounts, {
-        Account: accountName,
-      });
-      if (!sharedAccount) {
-        throw new Error(`No account found with the name \`${accountName}\`.`);
-      }
-
-      sharedToon.Account = sharedAccount;
-      await AppDataSource.manager.save(sharedToon);
-
-      await interaction.reply({
-        content: `The account for \`${toonName}\` has been updated to \`${accountName}\`.`,
-        ephemeral: false,
-      });
-    }
-    else if (sharedToon) {
-      // Display the linked account info
-      if (!sharedToon.Account) {
-        throw new Error(`Toon \`${toonName}\` is not linked to any account.`);
-      }
-      const accountInfo = await AppDataSource.manager.findOneBy(SharedAccounts, {
-        Account: sharedToon.Account.Account,
-      });
-      if (!accountInfo) {
-        throw new Error(`Account information for \`${toonName}\` could not be retrieved.`);
-      }
-      await interaction.reply({
-        content: `Toon: \`${toonName}\`\nAccount: \`${accountInfo.Account}\`\nPassword: \`${accountInfo.Password ?? 'N/A'}\``,
+        content: response,
         ephemeral: true,
+      });
+      await interaction.followUp({
+        content: `The password for account \`${account}\` has been set/updated.`,
       });
     }
     else {
-      throw new Error(
-        `Toon \`${toonName}\` does not exist and no account name was provided to create one.`,
+      // Retrieve the account info
+      const sharedAccount: SharedAccounts | null = await AppDataSource.manager.findOneBy(
+        SharedAccounts,
+        {
+          Account: account,
+        },
       );
+      if (!sharedAccount) {
+        throw new Error(`No account found with the name \`${account}\`.`);
+      }
+      await interaction.reply({
+        content: `Account: \`${account}\`\nPassword: \`${sharedAccount.Password}\``,
+        ephemeral: true,
+      });
     }
   }
   catch (error) {
     if (error instanceof Error) {
-      await interaction.reply({ content: error.message, ephemeral: true });
+      return interaction.reply({ content: error.message, ephemeral: true });
     }
   }
 }

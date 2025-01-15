f63c4c79bdea0228385d7256b1d156fe7cda436b
Ryan David Ward <ryandward@gmail.com>
2025-01-15 01:51:22 -0600

    get rid of wiki stuff

--- commands/bank/find.ts
+++ commands/bank/find.ts
@@ -1,7 +1,5 @@
-import * as Canvas from '@napi-rs/canvas';
 import {
   ActionRowBuilder,
-  AttachmentBuilder,
   AutocompleteInteraction,
   ButtonBuilder,
   ButtonStyle,
@@ -14,7 +12,7 @@ import {
 import _ from 'lodash';
 import { AppDataSource } from '../../app_data.js';
 import { Bank } from '../../entities/Bank.js';
-import { formatField, getImageUrl, getSpellDescription, getStatsBlock } from './item_functions.js';
+import { formatField } from './item_functions.js';
 
 export const data = new SlashCommandBuilder()
   .setName('find')
@@ -65,190 +63,101 @@ export async function execute(interaction: CommandInteraction) {
     if (!options.get('item')) {
       throw new Error('You must provide an item to find.');
     }
-    else {
-      const itemName = options.get('item')?.value as string;
 
-      const itemData = await AppDataSource.manager.find(Bank, {
-        where: { Name: itemName },
-      });
+    const itemName = options.get('item')?.value as string;
+    const itemData = await AppDataSource.manager.find(Bank, {
+      where: { Name: itemName },
+    });
 
-      // check if the itemName is a spell or a song
-      let itemText: string | null | undefined = '';
+    if (itemData.length === 0) {
+      await interaction.reply({ content: 'Item not found in bank.', ephemeral: true });
+      return;
+    }
 
-      if (itemName.startsWith('Spell: ') || itemName.startsWith('Song: ')) {
-        // const spellLevels = await getSpellLevels(itemName);
-        itemText = await getSpellDescription(itemName);
+    const request = new ButtonBuilder()
+      .setCustomId('request')
+      .setLabel('Request')
+      .setStyle(ButtonStyle.Success);
+
+    const cancel = new ButtonBuilder()
+      .setCustomId('cancel')
+      .setLabel('Cancel')
+      .setStyle(ButtonStyle.Danger);
+
+    const row = new ActionRowBuilder<ButtonBuilder>().addComponents(request, cancel);
+
+    const isStack = itemData.some(item => item.Quantity > 1);
+    const bankers = itemData.map(item => item.Banker);
+
+    const embed = new EmbedBuilder()
+      .setTitle(':bank: Bank Record')
+      .setDescription(`**${itemName}**\n<t:${Math.floor(Date.now() / 1000)}:R>`)
+      .setColor('Green');
+
+    const embedBuilder = [...new Set(bankers)].reduce((currentEmbed: EmbedBuilder, banker) => {
+      const itemsOnBankers = itemData.filter(item => item.Banker === banker);
+      const sortedItems = itemsOnBankers.sort((a, b) => a.Location.localeCompare(b.Location));
+      const sortedItemLocations = formatField(
+        sortedItems.map(item => _.replace(item.Location, '-', ' ')),
+      );
+
+      if (sortedItems.length === 0) {
+        return currentEmbed;
       }
-      else {
-        itemText = await getStatsBlock(itemName);
+
+      currentEmbed.addFields(
+        {
+          name: `:bust_in_silhouette: ${banker}`,
+          value: `${sortedItems.length} matching item(s).`,
+          inline: false,
+        },
+        { name: ':mag: Location', value: sortedItemLocations, inline: true },
+      );
+      if (isStack) {
+        currentEmbed.addFields({
+          name: ':money_bag: Stack',
+          value: formatField(sortedItems.map(item => item.Quantity.toString())),
+          inline: true,
+        });
       }
+      return currentEmbed;
+    }, embed);
 
-      if (!itemText) {
-        await interaction.reply({ content: 'Item not found.', ephemeral: true });
-        return;
-      }
+    await interaction.reply({ embeds: [embedBuilder], components: [row] });
 
-      const request = new ButtonBuilder()
-        .setCustomId('request')
-        .setLabel('Request')
-        .setStyle(ButtonStyle.Success);
+    const filter = (i: { customId: string }) => i.customId === 'request' || i.customId === 'cancel';
 
-      const cancel = new ButtonBuilder()
-        .setCustomId('cancel')
-        .setLabel('Cancel')
-        .setStyle(ButtonStyle.Danger);
+    try {
+      const collected = await interaction.channel?.awaitMessageComponent({ filter, time: 60000 });
 
-      const row = new ActionRowBuilder<ButtonBuilder>().addComponents(request, cancel);
-
-      itemText = itemText?.replace(/\[\[[^\]]*\|([^\]]+)\]\]/g, '$1');
-      itemText = itemText?.replace(/[[\]*]/g, '');
-      itemText = itemText?.replace(/(.{1,35})(\s|$)/g, '$1\n');
-
-      const lineHeight = 25;
-      const lines = itemText.split('\n');
-      const textHeight = lines.length * lineHeight;
-      const padding = 50;
-      const canvasHeight = _.max([textHeight + padding / 2, 200]) as number;
-      const canvas = Canvas.createCanvas(715, canvasHeight);
-      const context = canvas.getContext('2d');
-
-      const background = await Canvas.loadImage('./images/stars.png');
-      context.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-      const imageUrl = await getImageUrl(itemName);
-
-      if (imageUrl) {
-        try {
-          const icon = await Canvas.loadImage(imageUrl);
-          context.fillStyle = 'rgba(255, 255, 255, 0.1)';
-
-          const iconPadding = padding / 2;
-
-          // Calculate the icon size and position
-          const iconSize = 85;
-
-          // Calculate the background size and position
-          const bgSize = iconSize + 2 * iconPadding;
-          const bgX = canvas.width - bgSize - padding / 2;
-          const bgY = padding / 2;
-
-          // Calculate the icon position from the background position
-          const iconX = bgX + iconPadding;
-          const iconY = bgY + iconPadding;
-
-          // Define the corner radius
-          const cornerRadius = 10;
-
-          // Start a new path
-          context.beginPath();
-
-          // Draw the rounded rectangle
-          context.moveTo(bgX + cornerRadius, bgY);
-          context.arcTo(bgX + bgSize, bgY, bgX + bgSize, bgY + bgSize, cornerRadius);
-          context.arcTo(bgX + bgSize, bgY + bgSize, bgX, bgY + bgSize, cornerRadius);
-          context.arcTo(bgX, bgY + bgSize, bgX, bgY, cornerRadius);
-          context.arcTo(bgX, bgY, bgX + bgSize, bgY, cornerRadius);
-          context.closePath();
-
-          // Fill the path
-          context.fill();
-
-          // Draw the icon on top of the gray rectangle
-          context.drawImage(icon, iconX, iconY, iconSize, iconSize);
-          if (itemText) {
-            context.font = `${lineHeight}px sans-serif`;
-            context.fillStyle = '#ffffff';
-            lines.forEach((line, i) => {
-              context.fillText(line.trim(), 25, 25 + i * lineHeight + 25);
+      if (collected) {
+        if (collected.customId === 'request') {
+          await collected.update({ content: 'Requesting item...', components: [] });
+          const requestChannel = interaction.client.channels.cache.get('1213309735886000159');
+          if (requestChannel && interaction.member) {
+            // Send the request with the original embeds to the request channel
+            await (requestChannel as TextChannel).send({
+              content:
+                '<@&875884412259143711>' +
+                '\n' +
+                `Request for **${itemName}** by ${(interaction.member as GuildMember).displayName}`,
+              embeds: [embedBuilder],
             });
           }
+          await interaction.deleteReply();
         }
-        catch (error) {
-          console.error('Failed to load image:', error);
-          return;
-        }
-      }
-
-      const attachment = new AttachmentBuilder(await canvas.encode('png'), {
-        name: 'item-stats-image.png',
-      });
-
-      const isStack = itemData.some(item => item.Quantity > 1);
-      const bankers = itemData.map(item => item.Banker);
-
-      const embed = new EmbedBuilder()
-        .setTitle(':bank: Bank Record')
-        .setDescription(`**${itemName}**\n<t:${Math.floor(Date.now() / 1000)}:R>`)
-        .setColor('Green')
-        .setImage('attachment://item-stats-image.png');
-
-      const embedBuilder = [...new Set(bankers)].reduce((currentEmbed: EmbedBuilder, banker) => {
-        const itemsOnBankers = itemData.filter(item => item.Banker === banker);
-        const sortedItems = itemsOnBankers.sort((a, b) => a.Location.localeCompare(b.Location));
-        const sortedItemLocations = formatField(
-          sortedItems.map(item => _.replace(item.Location, '-', ' ')),
-        );
-
-        if (sortedItems.length === 0) {
-          return currentEmbed;
-        }
-
-        currentEmbed.addFields(
-          {
-            name: `:bust_in_silhouette: ${banker}`,
-            value: `${sortedItems.length} matching item(s).`,
-            inline: false,
-          },
-          { name: ':mag: Location', value: sortedItemLocations, inline: true },
-        );
-        if (isStack) {
-          currentEmbed.addFields({
-            name: ':money_bag: Stack',
-            value: formatField(sortedItems.map(item => item.Quantity.toString())),
-            inline: true,
+        else if (collected.customId === 'cancel') {
+          await interaction.deleteReply();
+          await interaction.followUp({
+            content: `Request for **${itemName}** cancelled.`,
+            ephemeral: true,
           });
         }
-        return currentEmbed;
-      }, embed);
-
-      await interaction.reply({ embeds: [embedBuilder], files: [attachment], components: [row] });
-
-      const filter = (i: { customId: string }) =>
-        i.customId === 'request' || i.customId === 'cancel';
-
-      try {
-        const collected = await interaction.channel?.awaitMessageComponent({ filter, time: 60000 });
-
-        if (collected) {
-          if (collected.customId === 'request') {
-            await collected.update({ content: 'Requesting item...', components: [] });
-            const requestChannel = interaction.client.channels.cache.get('1213309735886000159');
-            if (requestChannel && interaction.member) {
-              // Send the request with the original embeds to the request channel
-              await (requestChannel as TextChannel).send({
-                content:
-                  '<@&875884412259143711>' +
-                  '\n' +
-                  `Request for **${itemName}** by ${(interaction.member as GuildMember).displayName}`,
-                embeds: [embedBuilder],
-                files: [attachment],
-              });
-            }
-            await interaction.deleteReply();
-          }
-          else if (collected.customId === 'cancel') {
-            await interaction.deleteReply();
-            await interaction.followUp({
-              content: `Request for **${itemName}** cancelled.`,
-              ephemeral: true,
-            });
-          }
-        }
       }
-      catch (error) {
-        if (error instanceof Error) {
-          console.error('Error in awaitMessageComponent:', error);
-        }
+    }
+    catch (error) {
+      if (error instanceof Error) {
+        console.error('Error in awaitMessageComponent:', error);
       }
     }
   }

0038d5b444f707e889c14086fce686d6225448cd
Ryan David Ward <ryandward@gmail.com>
2025-01-15 01:53:42 -0600

    Refactor Census entity: remove duplicate Id column and adjust primary key definition

--- entities/Census.ts
+++ entities/Census.ts
@@ -1,9 +1,11 @@
 import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Index('idx_17818_census_pkey', ['Id'], { unique: true })
-@Index('idx_17818_sqlite_autoindex_census_1', ['Name'], { unique: true })
 @Entity('census', { schema: 'public' })
 export class Census {
+  @PrimaryGeneratedColumn({ type: 'bigint', name: 'id' })
+  Id: number;
+
   @Column('text', { name: 'discord_id' })
   DiscordId: string;
 
@@ -21,7 +23,4 @@ export class Census {
 
   @Column('text', { name: 'time' })
   Time: Date;
-
-  @PrimaryGeneratedColumn({ type: 'bigint', name: 'id' })
-  Id: number;
 }

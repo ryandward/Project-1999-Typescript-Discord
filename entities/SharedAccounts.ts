336e81ca7699b78716ae83ba2b0ca535f271677c
Ryan David Ward <ryandward@gmail.com>
2025-01-15 01:54:13 -0600

    get rid of SharedToons and SharedAccounts, for now

--- entities/SharedAccounts.ts
+++ entities/SharedAccounts.ts
@@ -1,23 +1,23 @@
-import { Column, Entity, Index, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { SharedToons } from './SharedToons.js';
+// import { Column, Entity, Index, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+// import { SharedToons } from './SharedToons.js';
 
-@Index('shared_accounts_account_key', ['Account'], { unique: true })
-@Index('shared_accounts_pkey', ['Id'], { unique: true })
-@Entity('shared_accounts', { schema: 'public' })
-export class SharedAccounts {
-  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
-  Id: number;
+// @Index('shared_accounts_account_key', ['Account'], { unique: true })
+// @Index('shared_accounts_pkey', ['Id'], { unique: true })
+// @Entity('shared_accounts', { schema: 'public' })
+// export class SharedAccounts {
+//   @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+//   Id: number;
 
-  @Column('character varying', { name: 'account', unique: true, length: 255 })
-  Account: string;
+//   @Column('character varying', { name: 'account', unique: true, length: 255 })
+//   Account: string;
 
-  @Column('character varying', { name: 'password', length: 255 })
-  Password: string;
+//   @Column('character varying', { name: 'password', length: 255 })
+//   Password: string;
 
-  @Column('character varying', { name: 'role', length: 50 })
-  Role: string;
+//   @Column('character varying', { name: 'role', length: 50 })
+//   Role: string;
 
-  // eslint-disable-next-line no-shadow
-  @OneToMany(() => SharedToons, SharedToons => SharedToons.Account)
-  SharedToons: SharedToons[];
-}
+//   // eslint-disable-next-line no-shadow
+//   @OneToMany(() => SharedToons, SharedToons => SharedToons.Account)
+//   SharedToons: SharedToons[];
+// }
--- entities/SharedToons.ts
+++ entities/SharedToons.ts
@@ -1,18 +1,18 @@
-import { Column, Entity, Index, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { SharedAccounts } from './SharedAccounts.js';
+// import { Column, Entity, Index, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+// import { SharedAccounts } from './SharedAccounts.js';
 
-@Index('shared_toons_pkey', ['Id'], { unique: true })
-@Index('shared_toons_name_key', ['Name'], { unique: true })
-@Entity('shared_toons', { schema: 'public' })
-export class SharedToons {
-  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
-  Id: number;
+// @Index('shared_toons_pkey', ['Id'], { unique: true })
+// @Index('shared_toons_name_key', ['Name'], { unique: true })
+// @Entity('shared_toons', { schema: 'public' })
+// export class SharedToons {
+//   @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+//   Id: number;
 
-  @Column('character varying', { name: 'name', unique: true, length: 255 })
-  Name: string;
+//   @Column('character varying', { name: 'name', unique: true, length: 255 })
+//   Name: string;
 
-  // eslint-disable-next-line no-shadow
-  @ManyToOne(() => SharedAccounts, SharedAccounts => SharedAccounts.SharedToons)
-  @JoinColumn([{ name: 'account', referencedColumnName: 'Account' }])
-  Account: SharedAccounts;
-}
+//   // eslint-disable-next-line no-shadow
+//   @ManyToOne(() => SharedAccounts, SharedAccounts => SharedAccounts.SharedToons)
+//   @JoinColumn([{ name: 'account', referencedColumnName: 'Account' }])
+//   Account: SharedAccounts;
+// }
